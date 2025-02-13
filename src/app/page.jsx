a405392124c5f1137fd9e2dd44989857baba5bdd
Artur <121varik121@example.com>
2025-02-14 01:09:11 +0200

    second commit

---
+++ src/app/page.jsx
@@ -0,0 +1,19 @@
+'use client'
+
+import { useState } from "react";
+import styles from "./page.module.css";
+import Valentinks from "./valentinks";
+
+export default function Home() {
+  const [isValet, setIsValet] = useState(false);
+
+  return (
+    <div className={styles.block}>
+      <div onClick={() => setIsValet(!isValet)} className={styles.text2}>Від zxrtw_1<div className={styles.heart1}></div></div>
+
+      {isValet ? (<Valentinks />) : null}
+
+    </div >
+
+  );
+}
