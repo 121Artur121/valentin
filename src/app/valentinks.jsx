8352807c654212f48d224b47cf1cfd7d5fa9178c
Artur <121varik121@example.com>
2025-02-14 01:17:23 +0200

    hot fix

--- src/app/valentinks.jsx
+++ src/app/valentinks.jsx
@@ -6,6 +6,7 @@ import { useState } from "react";
 export default function Valentinks() {
 
   const [isHovered, setIsHovered] = useState(false);
+
   return (
     <div
       className={styles.container}
